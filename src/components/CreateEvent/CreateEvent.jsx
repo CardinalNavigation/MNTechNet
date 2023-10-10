909c46a3132de2d1b79dab9afa1c87d8a1559800
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-10-10 16:10:08 -0500

    creating form pieces for our Create even, so that we can get our Saga up and working, now working on function to gatehr that information so it can be dispatched

--- src/components/CreateEvent/CreateEvent.jsx
+++ src/components/CreateEvent/CreateEvent.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import { useState } from "react";
 
-function CreateEvent (){
+function CreateEvent() {
+
+    const [eventName, setEventName] = useState('');
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const [address, setAddress] = useState('');
+    const [notes, setNotes] = useState('');
+
+    const handleSubmit = () => {
+        event.preventDefault()
+
+    }
 
     return (
-        <>
-        <p>Create Event</p>
-        </>
+        <section>
+            <div>
+                <h2>Create Event</h2>
+            </div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input value={eventName} placeholder="Name of Event" type="text" onChange={(event) => setEventName(event.target.value)}></input>
+                    <input value={date} placeholder="Date" type="text" onChange={(event) => setDate(event.target.value)}></input>
+                    <input value={time} placeholder="Time" type="text" onChange={(event) => setTime(event.target.value)}></input>
+                    <input value={address} placeholder="Address" type="text" onChange={(event) => setAddress(event.target.value)}></input>
+                    <input value={notes} placeholder="Notes" type="text" onChange={(event) => setNotes(event.target.value)}></input>
+                    <button type='submit'>Submit</button>
+                </form>
+                <button>Nevermind</button>
+            </div>
+        </section>
     )
 }
 
