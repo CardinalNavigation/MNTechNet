e785005940e0e4bf79ff9299d84bdc66630afd18
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-10-11 10:31:08 -0500

    i should have all that I need in my component for updating the profile, moving to add a function in the reducer, and in the server so that it hits the database

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,8 +7,8 @@ import { useHistory } from "react-router-dom";
 function Profile() {
 
     const user = useSelector((store) => store.user)
-    let userId=user.username
-    const [username, setUsername] = useState(userId);
+    let userID=user.id
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [passwordConfirm, setPasswordConfirm] = useState('');
     const [email, setEmail] = useState('');
@@ -17,14 +17,21 @@ function Profile() {
     const history = useHistory();
 
     const handleSubmit = () => {
-        console.log("click works")
         event.preventDefault()
 
+        if (password === passwordConfirm){
+            setPassword(password)
+        } else{
+            alert("Passwords Do Not Match")
+        }
+
         let profileUpdateData = {
+            userID: userID,
             username: username,
             email: email,
             password: password
         }
+        console.log("Profile Update Object Looks Like", profileUpdateData)
 
         dispatch({
             type: 'UPDATE_USER_INFO',
@@ -35,8 +42,8 @@ function Profile() {
     return (
         <>
             <h2>Edit Profile</h2>
-            <form>
-                <input value={username} placeholder="Username" type="text"></input>
+            <form onSubmit={handleSubmit}>
+                <input value={username} placeholder="Username" type="text" onChange={(event) => setUsername(event.target.value)}></input>
                 <input value={email} placeholder="E-mail Address" type="text" onChange={(event) => setEmail(event.target.value)}></input>
                 <input value={password} placeholder="Password" type="text" onChange={(event) => setPassword(event.target.value)}></input>
                 <input value={passwordConfirm} placeholder="Confirm Password" type="text" onChange={(event) => setPasswordConfirm(event.target.value)}></input>
