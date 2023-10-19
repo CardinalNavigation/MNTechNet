dd4a1504b2ab214eea77538d2998d22cba1802ae
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-10-18 23:19:58 -0500

    updating stylign for people edit modal, I had to remove the notes title about that input field, as it was auto fillign and stretching the modal so that you couldnt see anything in it

--- src/components/PeopleEditModal/PeopleEditModal.jsx
+++ src/components/PeopleEditModal/PeopleEditModal.jsx
@@ -1,3 +1,4 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -26,14 +27,14 @@ export default function PeopleEditModal(props) {
 
 
         let personUpdateData = {
-            id:props.person.id,
+            id: props.person.id,
             name: name,
             date: date,
             company: company,
             phone: phone,
             notes: notes,
             followUpDate: followUpDate,
-            userId:userId
+            userId: userId
         }
         console.log("Profile Update Object Looks Like", personUpdateData)
 
@@ -50,23 +51,36 @@ export default function PeopleEditModal(props) {
             <div className="modal">
                 <div className="overlay">
                     <div className="modal-content">
-                        <h1>Edit This Person's Details</h1>
-                        <form onSubmit={handleSubmit}>
-                            <h4>Name: {props.person.name}</h4>
-                            <input value={name} placeholder="Name" type="text" onChange={(event) => setName(event.target.value)}></input>
-                            <h4>Date: {props.person.formatted_date}</h4>
-                            <input value={date} placeholder="mm/dd/yyyy" type="text" onChange={(event) => setDate(event.target.value)}></input>
-                            <h4>Company: {props.person.company}</h4>
-                            <input value={company} placeholder="Company" type="text" onChange={(event) => setCompany(event.target.value)}></input>
-                            <h4>Phone: {props.person.phone}</h4>
-                            <input value={phone} placeholder="Phone" type="text" onChange={(event) => setPhone(event.target.value)}></input>
-                            <h4>Notes: {props.person.notes}</h4>
-                            <input value={notes} placeholder="Notes" type="text" onChange={(event) => setNotes(event.target.value)}></input>
-                            <h4>Follow-Up Date: {props.person.follow_up_date}</h4>
-                            <input value={followUpDate} placeholder="mm/dd/yyyy" type="text" onChange={(event) => setFollowUpDate(event.target.value)}></input>
-                            <button type='submit'>Save</button>
-                            <button type='nevermind' onClick={props.onClose}>Nevermind</button>
-                        </form>
+                        <Typography variant="h3" color="#235179">Edit This Person's Details</Typography>
+                        <Box display="flex"
+                            flexDirection="column"
+                            alignContent="center">
+                            <form onSubmit={handleSubmit}>
+                                <Box display="flex"
+                                    flexDirection="column"
+                                    alignItems="center" >
+                                    <Box display="flex"
+                                        flexDirection="column">
+                                        <h4>Name: {props.person.name}</h4>
+                                        <input value={name} placeholder="Name" type="text" onChange={(event) => setName(event.target.value)}></input>
+                                        <h4>Date: {props.person.formatted_date}</h4>
+                                        <input value={date} placeholder="mm/dd/yyyy" type="text" onChange={(event) => setDate(event.target.value)}></input>
+                                        <h4>Company: {props.person.company}</h4>
+                                        <input value={company} placeholder="Company" type="text" onChange={(event) => setCompany(event.target.value)}></input>
+                                        <h4>Phone: {props.person.phone}</h4>
+                                        <input value={phone} placeholder="Phone" type="text" onChange={(event) => setPhone(event.target.value)}></input>
+                                        <h4>Notes:</h4>
+                                        <input value={notes} placeholder="Notes" type="text" onChange={(event) => setNotes(event.target.value)}></input>
+                                        <h4>Follow-Up Date: {props.person.follow_up_date}</h4>
+                                        <input value={followUpDate} placeholder="mm/dd/yyyy" type="text" onChange={(event) => setFollowUpDate(event.target.value)}></input>
+                                    </Box>
+                                    <Box>
+                                        <button type='submit'>Save</button>
+                                        <button type='nevermind' onClick={props.onClose}>Nevermind</button>
+                                    </Box>
+                                </Box>
+                            </form>
+                        </Box>
                     </div>
                 </div>
             </div>
